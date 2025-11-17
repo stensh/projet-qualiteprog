7c7921192d59e35f3840813612a3442e43f9fb0c
sten <t.vuffray@gmail.com>
2025-11-17 09:19:08 +0100

    changement: oubli de changement de nom d'un fichier

--- evaluationAdaptative.cpp
+++ evaluationAdaptative.cpp
@@ -2,5 +2,8 @@
 
 namespace test
 {
-
+    void evaluationAdaptative::commencer(const int /*Questionnaire&*/ q) const
+    {
+        // tirer un nombre aléatoire pour choisir la première question à poser
+    }
 } // test
