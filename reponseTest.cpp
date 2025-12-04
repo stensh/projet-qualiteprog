086be56472b35a3ef9d9e9cc369e9f9b2cf8edae
JEDIDJA LE SAGE SURABONDANCE <surabondancenicolas@gmail.com>
2025-12-04 14:55:01 +0100

    #Ajout des tests unitaires de la classe reponse( vérificationd de la construction et du changement de validité fonctionnels)

--- reponseTest.cpp
+++ reponseTest.cpp
@@ -1,3 +1,21 @@
-//
-// Created by surab on 11/27/2025.
-//
+#include  "doctest.h"
+#include  "reponse.h"
+
+#include  <string>
+
+TEST_CASE("La réponse est bien construite et opérationel")
+{
+    std::string s = "Hello World";
+    test::reponse rep{s};
+    SUBCASE("Les données testées sont bien assignées")
+    {
+        REQUIRE_EQ(rep.donnee(),s);
+        REQUIRE_FALSE(rep.valide());
+    }
+    SUBCASE("Le changement de validité est opérationnel")
+    {
+        bool validite{false};
+        rep.changeValidite(validite);
+        REQUIRE_EQ(rep.valide(),validite);
+    }
+}
