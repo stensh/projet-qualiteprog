35c0f102dc284aa680e4efa1feba52bbbdfe6734
JEDIDJA LE SAGE SURABONDANCE <surabondancenicolas@gmail.com>
2025-12-04 17:05:27 +0100

    #Ajout des tests unitaires(test du constructeur et ajout d'une question dans le tableau de questions, sous cas des différentes questions) dans le fichier questionnaireTest.cpp

--- questionnaireTest.cpp
+++ questionnaireTest.cpp
@@ -1,3 +1,57 @@
-//
-// Created by surab on 11/27/2025.
-//
+#include "doctest.h"
+#include "questionnaire.h"
+
+#include <memory>
+#include <string>
+
+#include "questionChoixMultiples.h"
+#include "questionNumerique.h"
+#include "questionTexte.h"
+
+
+
+TEST_CASE("Le questionnaire est bien construit")
+{
+    std::string nomFichier{};
+    sujet::questionnaire q{nomFichier};
+    SUBCASE("Les données sont bien initialisées")
+    {
+       REQUIRE_EQ(nomFichier,q.nomFichier());
+       REQUIRE_FALSE(q.nombreQuestions());
+    }
+}
+
+TEST_CASE("L'ajout d'une question se déroule normalement")
+{
+    std::string nomFichier{};
+    sujet::questionnaire quest{nomFichier};
+
+
+    SUBCASE("La question ajouté est une question numérique")
+    {
+
+        quest.ajouteQuestion(std::make_unique<sujet::questionNumerique>("intitule","texte",45,50));
+
+        REQUIRE_EQ(quest.nombreQuestions(),1);
+        REQUIRE_NE(dynamic_cast<sujet::questionNumerique*>(&quest.questionCourante(0)),nullptr);
+    }
+
+
+    SUBCASE("La question ajouté est une question texte")
+    {
+        quest.ajouteQuestion(std::make_unique<sujet::questionTexte>("intitule","texte","reponse"));
+
+        sujet::questionTexte q{"","",""};
+         REQUIRE_EQ(quest.nombreQuestions(),1);
+         REQUIRE_NE(dynamic_cast<sujet::questionTexte*>(&quest.questionCourante(0)) ,nullptr);
+    }
+
+
+    SUBCASE("La question ajoutée est une question à choix multiples")
+    {
+        quest.ajouteQuestion(std::make_unique<sujet::questionChoixMultiples>("texte","intitule",2));
+
+        REQUIRE_EQ(quest.nombreQuestions(),1);
+        REQUIRE_NE(dynamic_cast<sujet::questionChoixMultiples*>(&quest.questionCourante(0)), nullptr);
+    }
+}
