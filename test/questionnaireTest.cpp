a15499054350f5bdc3ba9e1ef7d66fd6d079f1d7
quentin <rquentin.atexia@gmail.com>
2025-12-30 15:56:13 +0100

    Ajout des tests pour vider un questionnaire.

--- test/questionnaireTest.cpp
+++ test/questionnaireTest.cpp
@@ -1,23 +1,27 @@
-#include "test/doctest.h"
-#include "questionnaire/questionnaire.h"
-
 #include <memory>
 #include <string>
 
+#include "test/doctest.h"
+#include "questionnaire/questionnaire.h"
 #include "questionnaire/questionChoixMultiples.h"
 #include "questionnaire/questionNumerique.h"
 #include "questionnaire/questionTexte.h"
 
 
-
 TEST_CASE("Le questionnaire est bien construit")
 {
-    std::string nomFichier{};
-    sujet::questionnaire q{nomFichier};
-    SUBCASE("Les données sont bien initialisées")
+    SUBCASE("Questionnaire par défaut bien construit")
     {
-       REQUIRE_EQ(nomFichier,q.nomFichier());
-       REQUIRE_FALSE(q.taille());
+        sujet::questionnaire q{};
+        REQUIRE_EQ("",q.nomFichier());
+        REQUIRE_FALSE(q.taille());
+    }
+    SUBCASE("Questionnaire avec un nom de fichier bien construit")
+    {
+        std::string nomFichier{};
+        sujet::questionnaire q{nomFichier};
+        REQUIRE_EQ(nomFichier,q.nomFichier());
+        REQUIRE_FALSE(q.taille());
     }
 }
 
@@ -53,5 +57,21 @@ TEST_CASE("L'ajout d'une question se déroule normalement")
 
         REQUIRE_EQ(quest.taille(),1);
         REQUIRE_NE(dynamic_cast<sujet::questionChoixMultiples*>(quest.questionIndice(0).get()), nullptr);
+    }
+}
+
+TEST_CASE("Les questionnaire est bien vidé")
+{
+    sujet::questionnaire quest{};
+    SUBCASE("Un questionnaire vide est bien vidé")
+    {
+        quest.videQuestionnaire();
+        REQUIRE_EQ(quest.taille(),0);
+    }
+    SUBCASE("Un questionnaire contenant une question est bien vidé")
+    {
+        quest.ajouteQuestion(std::make_unique<sujet::questionNumerique>("intitule","texte",45,50));
+        quest.videQuestionnaire();
+        REQUIRE_EQ(quest.taille(),0);
     }
 }
