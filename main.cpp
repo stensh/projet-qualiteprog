a073b402168a6e2d8e4caef2b7343b0407f2a6e8
quentin <rquentin.atexia@gmail.com>
2025-12-30 17:05:01 +0100

    Modification des fonction pour éviter diverses erreurs.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <windows.h>
 
 
-void chargerQuestionnaire(sujet::questionnaire& q,int& code)
+void chargerQuestionnaire(sujet::questionnaire& q)
 {
     std::cout << "Veuillez rentrer le nom du questionnaire à charger : " << std::endl;
     std::string nomFichier;
@@ -13,12 +13,25 @@ void chargerQuestionnaire(sujet::questionnaire& q,int& code)
 
     q.modifierNom(nomFichier);
     sujet::gestionnaireQuestionnaire gestQ;
+    int code;
     gestQ.chargeQuestionnaire(q,code);
+    switch(code)
+    {
+        case 1: std::cout<<"Le fichier n'a pu être ouvert, essayé un autre fichier :\n";break;
+        case 2: std::cout<<"Le fichier n'est pas un fichier questionnaire, essayé un autre fichier :\n";break;
+        case 3: std::cout<<"Une erreur est survenu lors de la lecture du fichier, corrigé le fichier ou essayé un autre fichier :\n";break;
+    }
+
 }
 
 void apprendreQuestionnaire(const sujet::questionnaire& q)
 {
-    revision::apprentissage::commencer(q,std::cout,std::cin);
+    if (q.taille()>0) {
+        revision::apprentissage::commencer(q,std::cout,std::cin);
+    }
+    else {
+        std::cout<<"Le questionnaire ne contient aucune question\n";
+    }
 }
 
 int menu()
@@ -49,7 +62,7 @@ void run()
     {
         switch (menu())
         {
-        case 1:chargerQuestionnaire(quest,code); break;
+        case 1:chargerQuestionnaire(quest); break;
         case 2:apprendreQuestionnaire(quest); break;
         case 3:/* Afficher une liste des différentes évaluations*/ break;
         default: goOn=false;
