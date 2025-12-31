21ffaffe59013d09b0a13324b92c982186f91017
sten <t.vuffray@gmail.com>
2025-12-31 09:33:07 +0100

    modifs : revoir l'indentation

--- test/test_evaluation.cpp
+++ test/test_evaluation.cpp
@@ -4,7 +4,8 @@
 #include "../evaluation/evaluation.h"
 #include "../evaluation/evaluationTest.h"
 
-TEST_CASE("L'évaluation de base fonctionne") {
+TEST_CASE("L'évaluation de base fonctionne")
+{
     sujet::questionnaire q{""};
     q.ajouteQuestion(
         std::make_unique<sujet::questionTexte>(
@@ -14,17 +15,21 @@ TEST_CASE("L'évaluation de base fonctionne") {
         )
     );
     test::evaluationTest e{q};
-    SUBCASE("Le constructeur de l'evaluation fonctionne") {
+    SUBCASE("Le constructeur de l'evaluation fonctionne")
+    {
         REQUIRE_EQ(e.nbQuestions(), 1);
         REQUIRE_EQ(e.bonnesReponses(), 0);
         REQUIRE_EQ(e.questionsPosees(), 0);
     }
-    SUBCASE("L'incrémentation des compteurs fonctionne") {
-        SUBCASE("Incrémenter les bonnes réponses fonctionne") {
+    SUBCASE("L'incrémentation des compteurs fonctionne")
+    {
+        SUBCASE("Incrémenter les bonnes réponses fonctionne")
+        {
             e.incrementeBonnesReponses();
             REQUIRE_EQ(e.bonnesReponses(), 1);
         }
-        SUBCASE("Incrémenter les questions posées fonctionne") {
+        SUBCASE("Incrémenter les questions posées fonctionne")
+        {
             e.incrementeQuestionsPosees();
             REQUIRE_EQ(e.questionsPosees(), 1);
         }
