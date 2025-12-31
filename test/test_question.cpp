8943ae7f4f294b0ae0d7dae8b4696e1e22b1635c
sten <t.vuffray@gmail.com>
2025-12-31 10:18:57 +0100

    ajouts : Tests aux limites dans test_question.cpp et test des méthodes reponse() et contenu()

--- test/test_question.cpp
+++ test/test_question.cpp
@@ -21,26 +21,46 @@ TEST_CASE("La construction d'une question est correcte")
     {
         int min{45}, max{50};
         sujet::questionNumerique q{intitule,texte,min,max};
+
         SUBCASE("La construction de la partie question est correcte")
         {
             questionBienConstruite(q,intitule,texte);
         }
+
         SUBCASE("Les données de la question numérique sont bien assignées")
         {
             REQUIRE_EQ(q.limiteMin(),min);
             REQUIRE_EQ(q.limiteMax(),max);
             REQUIRE((q.reponseJuste(std::to_string(min+2))));
             REQUIRE_FALSE((q.reponseJuste(std::to_string(max+1))));
+            REQUIRE_FALSE((q.reponseJuste(std::to_string(min-1))));
+            // Test aux limites
+            REQUIRE((q.reponseJuste(std::to_string(min))));
+            REQUIRE((q.reponseJuste(std::to_string(max))));
+        }
+
+        SUBCASE("La méthode reponse() de questionNumerique fonctionne")
+        {
+            std::string reponseAttendue = "entre " + std::to_string(min) + " et " + std::to_string(max);
+            REQUIRE_EQ(q.reponse(), reponseAttendue);
+        }
+
+        SUBCASE("La méthode contenu() fonctionne")
+        {
+            std::string contenuAttendu = intitule + '\n' + texte;
+            REQUIRE_EQ(q.contenu(), contenuAttendu);
         }
     }
     SUBCASE("La construction d'une question à choix multiples est correcte")
     {
         int reponse{3};
         sujet::questionChoixMultiples q{intitule,texte,reponse};
+
         SUBCASE("La construction de la partie question est correcte")
         {
             questionBienConstruite(q,intitule,texte);
         }
+
         SUBCASE("Les données de la question à choix multiples sont bien assignées")
         {
             REQUIRE_EQ(std::stoi(q.reponse()),reponse);
@@ -56,6 +76,7 @@ TEST_CASE("La construction d'une question est correcte")
         {
             questionBienConstruite(q,intitule,texte);
         }
+        
         SUBCASE("Les données de la classe question à choix multiples")
         {
             REQUIRE_EQ(q.reponse(), reponse);
